302717814fa2618fc99a8acd8b9dd50a967d2efa
João Vitor Hatum Mendonça <joaovhm@gmail.com>
2018-06-03 23:24:14 -0300

    New implementation tests for VRP

--- week_7/vrp/solver.py
+++ week_7/vrp/solver.py
@@ -31,9 +31,11 @@ def solve_it(input_data):
     #the depot is always the first customer in the input
     depot = customers[0]
     
-    obj, vehicle_tours = trivial_solver(customers, depot, vehicle_count, vehicle_capacity)
+    #obj, vehicle_tours = trivial_solver(customers, depot, vehicle_count, vehicle_capacity)
     #obj, vehicle_tours = scip_solver(customers, customer_count, vehicle_count, vehicle_capacity)
-
+    #obj, vehicle_tours = scip_solver_2(customers, customer_count, vehicle_count, vehicle_capacity)
+    obj, vehicle_tours = scip_solver_3(customers, customer_count, vehicle_count, vehicle_capacity)
+    
     # prepare the solution in the specified output format
     outputData = '%.2f' % obj + ' ' + str(0) + '\n'
     if len(vehicle_tours) > 0:
@@ -82,7 +84,7 @@ def trivial_solver(customers, depot, vehicle_count, vehicle_capacity):
 
 def scip_solver(customers, customer_count, vehicle_count, vehicle_capacity):
     model = Model("vrp")
-    model.hideOutput()
+    #model.hideOutput()
     model.setMinimize()
 
     c_range = range(0, customer_count)
@@ -93,62 +95,90 @@ def scip_solver(customers, customer_count, vehicle_count, vehicle_capacity):
     for i in v_range:
         for j in c_range:
             # Variable: vehicle i visits customer j
-            x[i,j] = model.addVar(vtype="B", name="x(%s,%s)" % (i,j))
+            #x[i,j] = model.addVar(vtype="B", name="x(%s,%s)" % (i,j))
+       
+    #for i in v_range:
+        #for j in c_range:
             for k in c_range:
                 d[j,k] = length(customers[j], customers[k])
                 if k > j and j == 0:
                     # Variable: customer j is connected to customer k
-	            c[j,k] = model.addVar(ub=2, vtype="I", name="c(%s,%s)" % (j,k))
+                    c[j,k] = model.addVar(ub=2, vtype="I", name="c(%s,%s)" % (j,k))
                 elif k > j:
 	            c[j,k] = model.addVar(ub=1, vtype="I", name="c(%s,%s)" % (j,k))
-
+        # Constraint: the number of costumers visited by each vehicle cannot exceed its capacity
+	#model.addCons(quicksum(x[i,j]*customers[j].demand for j in c_range) <= vehicle_capacity, "cap(%s)" % i)
+    
     model.addCons(quicksum(c[0,k] for k in cd_range) == 2*vehicle_count, "DepotDegree")
     
-    for i in v_range:
+    #for i in v_range:
         # Constraint: the number of costumers visited by each vehicle cannot exceed its capacity
-	model.addCons(quicksum(x[i,j]*customers[j].demand for j in c_range) <= vehicle_capacity, "cap(%s)" % i)
+	#model.addCons(quicksum(x[i,j]*customers[j].demand for j in c_range) <= vehicle_capacity, "cap(%s)" % i)
         
         # Constraint: every vehicle must visit the first customer (depot)
         #model.addCons(x[i,0] > 0, "dep(%s)" % i)
     
     for j in cd_range:
         # Constraint: every customer has to be visited by exactly one vehicle
-        model.addCons(quicksum(x[i,j] for i in v_range) == 1, "visit(%s)" % j)
+        #model.addCons(quicksum(x[i,j] for i in v_range) == 1, "visit(%s)" % j)
 
         # Constraint: each customer has to have two connections
         model.addCons(quicksum(c[k,j] for k in c_range if k < j) + \
             quicksum(c[j,k] for k in c_range if k > j) == 2, "Degree(%s)"%j)
         
-    
+    '''
     for i in v_range:
         for j in c_range:   
              for k in c_range:
                 if k > j and j > 0:
                     # Constraint: if a vehicle visits two customers, they must be connected
                     model.addCons(x[i,j]*x[i,k] == c[j,k], "rel(%s, %s, %s)" %(i,j,k))
-    
+    '''    
+
     # Minimize: sum0..X(vehicleX(dist(depot, firstCustomer) + sum(dist(customerN, customerK)) + dist(lastCustomer, depot)))
-    model.setObjective(quicksum(d[j,k]*c[j,k] for j in c_range for k in c_range if k > j))
+    model.setObjective(quicksum(d[j,k]*c[j,k] for j in c_range for k in c_range if k > j), "minimize")
+
+    for (j,k) in d:
+        print "(%s,%s): %s" % (j,k,d[j,k])
 
     #model.data = x, c
     model.optimize()
-    best_sol = model.getBestSol()
+    #best_sol = model.getBestSol()
     vehicle_tours = []
+    added_indexes = []
     i = 0
     for j in cd_range:
         vehicle_tours.append([])
-        current_val = model.getSolVal(best_sol, c[0,j])
-        if current_val > 0:
+        #current_val = model.getSolVal(best_sol, c[0,j])
+        current_val = model.getVal(c[0,j])
+        if current_val > 0 and not j in added_indexes:
             vehicle_tours[i].append(customers[j])
+            added_indexes.append(j)
             for k in cd_range:
                 if k > j:
-                    current_val = model.getSolVal(best_sol, c[j,k])
-                    if current_val > 0:
+                    #current_val = model.getSolVal(best_sol, c[j,k])
+                    current_val = model.getVal(c[j,k])
+                    if current_val > 0 and not k in added_indexes:
                         vehicle_tours[i].append(customers[k])
+                        added_indexes.append(k)
+                    elif k < j:
+                        #current_val = model.getSolVal(best_sol, c[k,j])
+                        current_val = model.getVal(c[k,j])
+                        if current_val > 0 and not k in added_indexes:
+                            vehicle_tours[i].append(customers[k])
+                            added_indexes.append(k)
         i =+ 1
     
+    print vehicle_tours
+    
     for (j,k) in c:
-        print("(%s,%s): %s" % (j,k,model.getSolVal(best_sol, c[j,k])))
+        #print("c(%s,%s): %s" % (j,k,model.getSolVal(best_sol, c[j,k])))
+        print("c(%s,%s): %s" % (j,k,model.getVal(c[j,k])))
+
+    '''
+    for (i,j) in x:
+        print("x(%s,%s): %s" %(i,j,model.getSolVal(best_sol, x[i,j])))
+    '''
     '''
     for i in v_range:
         vehicle_tours.append([])
@@ -160,9 +190,152 @@ def scip_solver(customers, customer_count, vehicle_count, vehicle_capacity):
                 break
     print(vehicle_tours)
     '''
-    obj = model.getSolObjVal(best_sol)
 
+    #obj = model.getSolObjVal(best_sol)
+    obj = model.getObjVal()
+
+    return obj, vehicle_tours
+
+'''
+Source: https://github.com/SCIP-Interfaces/PySCIPOpt/blob/eb4792dbc05a443ab0263fbbf184011083710ba7/examples/unfinished/vrp.py
+'''
+def scip_solver_2(customers, customer_count, vehicle_count, vehicle_capacity):
+    def addcut(cut_edges):
+        """addcut: add constraint to eliminate infeasible solutions
+        """
+        G = networkx.Graph()
+        G.add_edges_from(cut_edges)
+        Components = networkx.connected_components(G)
+        cut = False
+        model.freeTransform()
+        for S in Components:
+            S_card = len(S)
+            q_sum = sum(customers[i].demand for i in S)
+            NS = int(math.ceil(float(q_sum)/vehicle_capacity))
+            S_edges = [(i,j) for i in S for j in S if i<j and (i,j) in cut_edges]
+            if S_card >= 3 and (len(S_edges) >= S_card or NS > 1):
+                add = model.addCons(quicksum(x[i,j] for i in S for j in S if j > i) <= S_card-NS)
+                cut = True
+        return cut
+
+    model = Model("vrp")
+
+    c_range = range(0, customer_count)
+    cd_range = range(1, customer_count)
+    v_range = range(0, vehicle_count)
+    
+    x, d = {}, {}
+    for i in c_range:
+        for j in c_range:
+            d[i,j] = length(customers[i], customers[j])
+            if j > i and i == 0:       # depot
+                x[i,j] = model.addVar(ub=2, vtype="I", name="x(%s,%s)"%(i,j))
+            elif j > i:
+                x[i,j] = model.addVar(ub=1, vtype="I", name="x(%s,%s)"%(i,j))
+    
+    model.addCons(quicksum(x[0,j] for j in cd_range) == 2*vehicle_count, "DegreeDepot")
+    for i in cd_range:
+        model.addCons(quicksum(x[j,i] for j in c_range if j < i) +
+                        quicksum(x[i,j] for j in c_range if j > i) == 2, "Degree(%s)"%i)
+
+    model.setObjective(quicksum(d[i,j]*x[i,j] for i in c_range for j in c_range if j>i), "minimize")
+
+    model.hideOutput()
+
+    EPS = 1.e-6
+    while True:
+        model.optimize()
+        edges = []
+        for (i,j) in x:
+            if model.getVal(x[i,j]) > EPS:
+                if i != 0 and j != 0:
+                    edges.append((i,j))
+        if addcut(edges) == False:
+            break
+
+    print edges
+    return model.getObjVal(),[]
+
+'''
+Source: https://scholarworks.waldenu.edu/cgi/viewcontent.cgi?article=1020&context=ijamt
+'''
+def scip_solver_3(customers, customer_count, vehicle_count, vehicle_capacity):
+    '''
+    #### Notations #### 
+    G = Symmetric Graph; G= (T, A) 
+    T = Set of Nodes; T = [N ∪ {0, n+1}] 
+    A = Set of Arcs linking any pair of nodes, (i.j) ∈ A
+    V = Total Number of Vehicles; v = {1,2...V} 
+    y[i,j] = Cost of travel from node i to node j 
+    d[i] = Delivery requests of node i, i = 1, ..., N
+    p[i] = Pickup requests of node i, i = 1, ..., N (NOT TO BE USED IN THIS PROBLEM)
+    Q = Capacity of Vehicle 
+    TL = Maximum Route Length for any Vehicle v
+
+    #### Decision Variables #### 
+    D[i,v] = The load remaining to be delivered by vehicle v when departing from node i
+    P[i,v] = The cumulative load picked by vehicle v when departing from node i (NOT TO BE USED IN THIS PROBLEM)
+    X[v,i,j] = 1 if vehicle travels from i to j, 0 otherwise
+
+    P[i], d[i], Q, y[i,j] are non-negative integers 
+
+    '''
+
+    model = Model("vrp")
+    #model.hideOutput()
+    model.setMinimize()
+
+    T = customers
+    V = vehicle_count
+    Q = vehicle_capacity
+    A = range(0, customer_count)
+    Am = range(1, customer_count)
+    Vr = range(0, vehicle_count)
+
+    y, d, D, X = {}, {}, {}, {}
+    for i in A:
+        d[i] = customers[i].demand
+        for j in A:
+            y[i,j] = length(customers[i], customers[j])
+            for v in Vr:
+                X[v,i,j] = model.addVar(vtype="B", name="X(%s,%s,%s)" % (v,i,j))
+        for v in Vr:
+	    D[i,v] = model.addVar(lb=0, ub=Q, vtype="I", name="D(%s,%s)" % (i,v))
+
+    for v in Vr:
+        model.addCons(D[0,v] == Q, "c5(%s)" % v)
+        for i in Am:
+            model.addCons(D[i,v] <= Q, "c4(%s,%s)" % (i,v))
+            #if i > 0:
+            model.addCons(D[i,v] >= 0, "c8(%s,%s)" % (i,v))
+        model.addCons(quicksum(X[v,0,j] for j in Am) <= 1, "c2(%s)" % v)
+        model.addCons(quicksum(X[v,i,j]*(d[i]+d[j]) for i in Am for j in Am) <= Q, "c6(%s)" % (v))
+
+    for j in A:
+        if j > 0:
+            model.addCons(quicksum(X[v,i,j] for v in Vr for i in A) == 1, "c1(%s)" % (j))
+        for v in Vr:
+            model.addCons(quicksum(X[v,i,j] for i in A) - \
+                quicksum(X[v,j,i] for i in A) == 0, "c3(%s,%s)" % (j,v))
+
+    for i in Am:
+        for j in Am:
+            for v in Vr:
+                model.addCons((D[i,v] - d[j] - D[j,v])*X[v,i,j] == 0, "c7(%s,%s,%s)" % (i,j,v))
+
+
+    model.setObjective(quicksum(y[i,j]*X[v,i,j] for i in A for j in A for v in Vr), "minimize")
+
+    model.optimize()
+    best_sol = model.getBestSol()
     vehicle_tours = []
+
+    for v in Vr:
+        for i in A:
+            for j in A:
+                print "X[%s,%s,%s]: %s" % (v,i,j,model.getSolVal(best_sol, X[v,i,j]))
+
+    obj = model.getSolObjVal(best_sol)
 
     return obj, vehicle_tours
 
